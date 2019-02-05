f4bd89de2a103b90e18e91e7c3e101314fd3dcec
Anton Betsun <messbees@gmail.com>
2019-02-05 17:27:35 +0300

    add constants

---
+++ app/src/main/java/com/messbees/smartaquarium/Constants.java
@@ -0,0 +1,10 @@
+package com.messbees.smartaquarium;
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://iot.eclipse.org:1883";
+
+    public static final String PUBLISH_TOPIC = "androidkt/topic";
+
+    public static final String CLIENT_ID = "androidkt";
+}
